55cfb4f6d8860b472e15cccff3381897fe168bd7
fegusta <fegtnc@gmail.com>
2022-03-17 13:39:30 -0300

    refactor mainActivity

--- app/src/main/java/com/fegusta/abcapacity/ui/MainActivity.kt
+++ app/src/main/java/com/fegusta/abcapacity/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.fegusta.abcapacity.adapter.LevelAdapter
@@ -13,23 +14,35 @@ import com.fegusta.abcapacity.ui.CadastroQuestActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var floatButton: FloatingActionButton
+    private lateinit var recyclerViewLayourRecycler: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        iniciarRecyclerView()
-
-        var floatButton = findViewById<FloatingActionButton>(R.id.floatButton)
-        floatButton.setOnClickListener(this)
-
+        initViews()
+        initListeners()
+        initRecyclerView()
     }
 
+    private fun initViews() {
+        floatButton = findViewById<FloatingActionButton>(R.id.floatButton)
 
-    private fun iniciarRecyclerView() {
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view_layour_recycler)
-        recyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        recyclerViewLayourRecycler = findViewById<RecyclerView>(R.id.recyclerViewLayourRecycler)
+    }
+
+    private fun initListeners() {
+        floatButton.setOnClickListener(this)
+    }
+
+    private fun initRecyclerView() {
+        GridLayoutManager(this,3, RecyclerView.VERTICAL, false).apply {
+            recyclerViewLayourRecycler.layoutManager = this
+        }
         val repo = QuestRepository(this)
-        recyclerView.adapter = QuestAdapter(repo.getQuests())
+        recyclerViewLayourRecycler.adapter = QuestAdapter(repo.getQuests())
     }
 
     override fun onClick(view: View?) {
