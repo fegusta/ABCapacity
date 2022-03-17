d1c88cb05d3489dc8041aeef12d793352bc81c1e
fegusta <fegtnc@gmail.com>
2022-03-17 18:37:16 -0300

    include bottom_navigation_menu

--- app/src/main/java/com/fegusta/abcapacity/ui/MainActivity.kt
+++ app/src/main/java/com/fegusta/abcapacity/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -11,6 +12,7 @@ import com.fegusta.abcapacity.adapter.LevelAdapter
 import com.fegusta.abcapacity.adapter.QuestAdapter
 import com.fegusta.abcapacity.repository.QuestRepository
 import com.fegusta.abcapacity.ui.CadastroQuestActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -18,26 +20,41 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var floatButton: FloatingActionButton
     private lateinit var recyclerViewLayourRecycler: RecyclerView
 
+    private lateinit var bottom_navigation: BottomNavigationView
+
     private lateinit var questRepository: QuestRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         initViews()
         initListeners()
         initObjects()
         initRecyclerView()
+        initSignalNotifications(R.id.menu_main)
     }
 
     private fun initViews() {
         floatButton = findViewById<FloatingActionButton>(R.id.floatButton)
+
+        bottom_navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
         recyclerViewLayourRecycler = findViewById<RecyclerView>(R.id.recyclerViewLayourRecycler)
     }
 
     private fun initListeners() {
         floatButton.setOnClickListener(this)
+
+        bottom_navigation.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.menu_add_quest -> {
+                    val intent = Intent(this, CadastroQuestActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     private fun initObjects() {
@@ -49,6 +66,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             recyclerViewLayourRecycler.layoutManager = this
         }
         recyclerViewLayourRecycler.adapter = QuestAdapter(questRepository.getQuests())
+    }
+
+    private fun initSignalNotifications(id: Int) {
+        var badge = bottom_navigation.getOrCreateBadge(id)
+        badge.isVisible = true
+        badge.number = 99
     }
 
     override fun onClick(v: View) {
